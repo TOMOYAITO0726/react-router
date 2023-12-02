977d954327d49c3ce706eb1284632e8ce704be30
Tomoya Ito <tomoyaito199507265724@gmail.com>
2023-12-02 15:13:44 +0900

    基本的なページ遷移

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
 import { Page2 } from "./Page2";
@@ -10,12 +10,23 @@ export default function App() {
       {/* //囲った配下でルーティングを有効にする */}
       <div className="App">
         <Link to="/">Home</Link> {/*Linkでページ遷移を設定できる */}
-        <Link to="/">Home</Link>
-        <Link to="/">Home</Link>
-        <Home />
-        <Page1 />
-        <Page2 />
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        
       </div>
+      <Switch>{/* Swictでコンポーネントを分ける */}
+      <Route exact path="/">{/* RouteでURLとコンポーネントを結びつける */}
+        <Home />
+      </Route>
+      <Route path="/page1">{/* RouteでURLとコンポーネントを結びつける */}
+        <Page1 />
+      </Route>
+      <Route path="/page2">{/* RouteでURLとコンポーネントを結びつける */}
+        <Page2 />
+      </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
