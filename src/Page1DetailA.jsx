0d7202323571f53badbdec361529b37ee2ebcc8c
Tomoya Ito <tomoyaito199507265724@gmail.com>
2023-12-05 21:54:23 +0900

    historyによるページ遷移

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,10 +1,15 @@
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useLocation } from "react-router-dom/cjs/react-router-dom.min";
 export const Page1DetailA = () => {
   const{ state }  = useLocation();
   console.log(state);  
+
+  const history = useHistory();
+
+  const onClickBack = () => history.goBack();//戻れるようになる
   return (
       <div>
         <h1>Page1DetailAページです</h1>
+        <button onClick={onClickBack}>戻る</button>
       </div>
     );
   };
